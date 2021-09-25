dbc8dbf22927181e2f39a4b86628a8f982c207a4
nedimazar <nedimazar@gmail.com>
2021-09-25 15:38:00 +0200

    Weird utf bug fix

--- neat_optimization.py
+++ neat_optimization.py
@@ -68,10 +68,12 @@ run_mode = 'train' # train or test
 
 
 # Load configuration.
-config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                     neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                     'config-feedforward')
-print(config)
+config = neat.Config(neat.DefaultGenome,
+    neat.DefaultReproduction,
+    neat.DefaultSpeciesSet,
+    neat.DefaultStagnation,
+    'config-feedforward')
+
 # Create the population, which is the top-level object for a NEAT run.
 p = neat.Population(config)
 # Add a stdout reporter to show progress in the terminal.
@@ -79,11 +81,6 @@ p.add_reporter(neat.StdOutReporter(True))
 
 # Run until a solution is found.
 winner = p.run(eval_genomes)
-
-# Display the winning genome.
-
-print('\nBest genome:\n{!s}'.format(winner))
-
 
 
 
